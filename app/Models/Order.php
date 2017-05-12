284899246f263a198da249d1cd5a52d70b792561
Antony Repin <lehcode@gmail.com>
2017-05-12 14:08:59 +0300

    Updated Order and Payment models

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-	//
+	protected $fillable = [
+		'departure_date',
+		'expected_delivery_date',
+		'recipient_name',
+		'customer_id',
+		'driver_id',
+		'shipment_id',
+		'route_id',
+		'trip_id',
+	];
+
+	protected $dates = ['deleted_at', 'created_at', 'updated_at'];
+
+	protected $rules = [
+		'name' => 'required',
+		'email' => 'required|email|unique:users,email',
+		'phone' => 'required|phone:AUTO,mobile|unique:users,phone',
+		'password' => 'required|min:5'
+	];
+
 }
