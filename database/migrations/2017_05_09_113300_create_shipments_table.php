4363b21ef4c5622f01bdfc375c2bc283dda212c5
Antony Repin <lehcode@gmail.com>
2017-05-12 13:30:10 +0300

    Added Shipment model

---
+++ database/migrations/2017_05_09_113300_create_shipments_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateShipmentsTable extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('shipments', function (Blueprint $table) {
+
+			$table->uuid('id');
+			$table->primary('id');
+
+			$table->string('name', 64);
+			$table->integer('height')->unsigned();
+			$table->integer('length')->unsigned();
+			$table->integer('width')->unsigned();
+			$table->integer('weight')->unsigned();
+			$table->integer('category_id')->unsigned();
+			$table->foreign('category_id')
+				->references('id')->on('shipment_categories');
+
+			$table->timestamps();
+			$table->softDeletes();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		DB::statement("SET FOREIGN_KEY_CHECKS=0");
+		Schema::drop('shipments');
+		DB::statement("SET FOREIGN_KEY_CHECKS=1");
+	}
+}
