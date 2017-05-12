7cd5ed75e29fe782009d218e12842fcf945294de
Antony Repin <lehcode@gmail.com>
2017-05-12 13:22:22 +0300

    Added City model with Saudi Arabia cities seed

---
+++ app/Models/City.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class City extends Model
+{
+	/**
+	 * @var array
+	 */
+	protected $fillable = ['name', 'status'];
+
+	/**
+	 * @var array
+	 */
+	protected $visible = ['id', 'name', 'status'];
+
+	/**
+	 * @var bool
+	 */
+	public $timestamps = false;
+
+
+}
---
+++ database/migrations/2017_04_29_235600_create_cities_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCitiesTable extends Migration
+{
+	protected $name = 'cities';
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create($this->name, function (Blueprint $table) {
+			$table->increments('id');
+			$table->string('name');
+			$table->boolean('active');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		DB::statement("SET FOREIGN_KEY_CHECKS=0");
+		Schema::drop($this->name);
+		DB::statement("SET FOREIGN_KEY_CHECKS=1");
+	}
+}
