821f7c4de2353b841295ef35f94ac8a7505b8e8d
Antony Repin <lehcode@gmail.com>
2017-05-31 06:38:19 +0300

    Added shipment categories seeder

---
+++ database/seeds/ShipmentCategoriesSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use App\Models\ShipmentCategory;
+
+class ShipmentCategoriesSeeder extends Seeder
+{
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		$this->command->info('Creating shipment categories');
+		$faker = Faker::create('en_GB');
+
+		ShipmentCategory::create(['name' => 'Documents', 'description' => $faker->sentence()]);
+		ShipmentCategory::create(['name' => 'Box', 'description' => $faker->sentence()]);
+		ShipmentCategory::create(['name' => 'Electronics', 'description' => $faker->sentence()]);
+		ShipmentCategory::create(['name' => 'Other', 'description' => 'None of the existing categories']);
+	}
+}
