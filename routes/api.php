117eefe6cf6768c6f24b48db9e29081fa85c447a
Antony Repin <lehcode@gmail.com>
2017-05-31 06:44:45 +0300

    Updated routes

--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,7 @@ Route::group(['middleware' => 'api'], function () {
 			Route::post('user/restore-password', ['uses' => 'RestorePasswordController@sendLink']);
 			Route::any('user/me', ['middleware' => 'jwt.auth', 'uses' => 'AuthController@me']);
 			Route::get('user/navigation', ['middleware' => 'jwt.auth', 'uses' => 'AuthController@navigation']);
+			Route::get('shipment-categories', ['middleware' => 'jwt.auth', 'uses' => 'ShipmentController@categories']);
 		});
 
 		/*
@@ -32,23 +33,25 @@ Route::group(['middleware' => 'api'], function () {
 				Route::post('edit', ['uses' => 'UserController@edit']);
 			});
 
-			Route::group(['middleware' => 'jwt.auth', 'namespace' => 'Customer'], function () {
+			Route::group(['middleware' => 'jwt.auth'], function () {
 				Route::get('navigation', ['uses' => 'CustomerController@navigation']);
 			});
 
-			Route::group(['prefix' => 'user', 'namespace' => 'Customer'], function () {
+			Route::group(['prefix' => 'user'], function () {
 				Route::post('create', ['uses' => 'CustomerController@create']);
 			});
 
-			Route::group(['prefix' => 'order', 'namespace' => 'Customer'], function () {
-				Route::get('list', ['uses' => 'OrderController@orders']);
-				Route::get('{order}', ['uses' => 'OrderController@order']);
+			Route::group(['prefix' => 'order'], function () {
+				Route::get('list', ['uses' => 'OrderController@all']);
+				Route::get('{order_id}', ['uses' => 'OrderController@item']);
+				Route::post('create', ['uses' => 'OrderController@create']);
 			});
 
-			Route::group(['prefix' => 'trip', 'namespace' => 'Customer'], function () {
-				Route::get('list', ['uses' => 'OrderController@trips']);
-				Route::get('{order}', ['uses' => 'OrderController@trip']);
+			Route::group(['prefix' => 'trip'], function () {
+				Route::get('list', ['uses' => 'TripController@all']);
+				Route::get('{trip_id}', ['uses' => 'TripController@item']);
 			});
+
 		});
 
 		/*
@@ -60,19 +63,27 @@ Route::group(['middleware' => 'api'], function () {
 				Route::post('edit', ['uses' => 'UserController@edit']);
 			});
 
-			Route::group(['prefix' => 'user', 'namespace' => 'Carrier'], function () {
+			Route::group(['prefix' => 'user'], function () {
 				Route::post('create', ['uses' => 'CarrierController@create']);
 			});
 
-			Route::group(['middleware' => 'jwt.auth', 'namespace' => 'Carrier'], function () {
+			Route::group(['middleware' => 'jwt.auth'], function () {
 				Route::get('navigation', ['uses' => 'CarrierController@navigation']);
 
-				Route::group(['prefix' => 'trip', 'namespace' => 'Carrier'], function () {
-					Route::get('list', ['uses' => 'TripController@list']);
-					Route::get('{trip}', ['uses' => 'TripController@trip']);
+				Route::group(['prefix' => 'trip'], function () {
+					Route::post('create', ['uses' => 'TripController@createTrip']);
+					Route::get('list', ['uses' => 'TripController@getTrips']);
+					Route::get('{trip_id}', ['uses' => 'TripController@getTrip']);
 				});
-
 			});
+
+			Route::group(['middleware' => 'jwt.auth'], function () {
+				Route::group(['prefix' => 'order'], function () {
+					Route::get('orders', ['uses' => 'OrderController@getOrders']);
+					Route::get('{order_id}', ['uses' => 'OrderController@getOrder']);
+				});
+			});
+
 		});
 	});
 });
