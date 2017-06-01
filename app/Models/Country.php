14b6b1bd95ef7b83cb534f454f08b11109ea1922
Antony Repin <lehcode@gmail.com>
2017-06-01 15:04:48 +0300

    Updated Country model

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -9,13 +9,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Country
+/**
+ * Class Country
+ * @package App\Models
+ */
+class Country extends Model
 {
 	/**
 	 * @var bool
 	 */
 	public $timestamps = false;
 
+	/**
+	 * @return \Illuminate\Database\Eloquent\Relations\HasMany
+	 */
 	public function cities()
 	{
 		return $this->hasMany('App\Models\City');
