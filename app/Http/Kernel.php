81c3d84445a88e7fa67fb13d81537314997bd70c
Antony Repin <lehcode@gmail.com>
2017-06-05 16:19:30 +0300

    Added middlewares to Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -5,70 +5,77 @@ namespace App\Http;
 use App\Http\Middleware\Locale;
 use App\Http\Middleware\Morph;
 use App\Http\Middleware\MorphDataUrls;
+use App\Http\Middleware\SetApiHost;
+use App\Http\Middleware\VerifyUuid;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
+use Barryvdh\Cors\HandleCors;
 
 class Kernel extends HttpKernel
 {
-    /**
-     * The application's global HTTP middleware stack.
-     *
-     * These middleware are run during every request to your application.
-     *
-     * @var array
-     */
-    protected $middleware = [
-        \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
-        \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
-        \App\Http\Middleware\TrimStrings::class,
-        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \Barryvdh\Cors\HandleCors::class,
-    ];
+	/**
+	 * The application's global HTTP middleware stack.
+	 *
+	 * These middleware are run during every request to your application.
+	 *
+	 * @var array
+	 */
+	protected $middleware = [
+		\Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+		\Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+		\App\Http\Middleware\TrimStrings::class,
+		\Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+		\Barryvdh\Cors\HandleCors::class,
+	];
 
-    /**
-     * The application's route middleware groups.
-     *
-     * @var array
-     */
-    protected $middlewareGroups = [
-        'web' => [
-            \App\Http\Middleware\EncryptCookies::class,
-            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            \Illuminate\Session\Middleware\StartSession::class,
-            // \Illuminate\Session\Middleware\AuthenticateSession::class,
-            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        ],
+	/**
+	 * The application's route middleware groups.
+	 *
+	 * @var array
+	 */
+	protected $middlewareGroups = [
+		'web' => [
+			\App\Http\Middleware\EncryptCookies::class,
+			\Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+			\Illuminate\Session\Middleware\StartSession::class,
+			//\Illuminate\Session\Middleware\AuthenticateSession::class,
+			\Illuminate\View\Middleware\ShareErrorsFromSession::class,
+			\App\Http\Middleware\VerifyCsrfToken::class,
+			\Illuminate\Routing\Middleware\SubstituteBindings::class,
+		],
 
-        'api' => [
-            'throttle:60,1',
-            'bindings',
-            \Barryvdh\Cors\HandleCors::class,
-            Morph::class,
-            Locale::class
-        ],
-    ];
+		'api' => [
+			'throttle:60,1',
+			'bindings',
+			HandleCors::class,
+			Morph::class,
+			Locale::class,
+			SetApiHost::class,
+			VerifyUuid::class,
+		],
+	];
 
-    /**
-     * The application's route middleware.
-     *
-     * These middleware may be assigned to groups or used individually.
-     *
-     * @var array
-     */
-    protected $routeMiddleware = [
-        'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
-        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-        'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        'can' => \Illuminate\Auth\Middleware\Authorize::class,
-        'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
-        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'role' => \Laratrust\Middleware\LaratrustRole::class,
-        'permission' => \Laratrust\Middleware\LaratrustPermission::class,
-        'ability' => \Laratrust\Middleware\LaratrustAbility::class,
-        'jwt.auth' => \Tymon\JWTAuth\Middleware\GetUserFromToken::class,
-        'jwt.refresh' => \Tymon\JWTAuth\Middleware\RefreshToken::class,
-        'user.type' => \App\Http\Middleware\UserType::class,
-        'maintenance' => \App\Http\Middleware\CheckMaintenance::class
-    ];
+	/**
+	 * The application's route middleware.
+	 *
+	 * These middleware may be assigned to groups or used individually.
+	 *
+	 * @var array
+	 */
+	protected $routeMiddleware = [
+		'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
+		'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+		'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
+		'can' => \Illuminate\Auth\Middleware\Authorize::class,
+		'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+		'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+		'role' => \Laratrust\Middleware\LaratrustRole::class,
+		'permission' => \Laratrust\Middleware\LaratrustPermission::class,
+		'ability' => \Laratrust\Middleware\LaratrustAbility::class,
+		'jwt.auth' => \Tymon\JWTAuth\Middleware\GetUserFromToken::class,
+		'jwt.refresh' => \Tymon\JWTAuth\Middleware\RefreshToken::class,
+		'user.type' => \App\Http\Middleware\UserType::class,
+		'maintenance' => \App\Http\Middleware\CheckMaintenance::class,
+		'api.host' => SetApiHost::class,
+		'api.uuid' => VerifyUuid::class,
+	];
 }
