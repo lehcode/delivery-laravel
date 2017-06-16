c71cda72335f0b46ce574da32b7fdf656b9ab60e
Antony Repin <lehcode@gmail.com>
2017-06-16 06:12:58 +0300

    Added hidden attributes to Role model

--- app/Models/User/Role.php
+++ app/Models/User/Role.php
@@ -4,7 +4,14 @@ namespace App\Models\User;
 
 use Laratrust\LaratrustRole;
 
+/**
+ * Class Role
+ * @package App\Models\User
+ */
 class Role extends LaratrustRole
 {
-    
+	/**
+	 * @var array
+	 */
+	protected $hidden = ['pivot', 'updated_at', 'id'];
 }
