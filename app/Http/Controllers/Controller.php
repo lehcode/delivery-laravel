6cf48029aa0bfbc327b4ada25de9d8fa9cef204c
Antony Repin <lehcode@gmail.com>
2017-07-05 07:36:53 +0300

    Removed swagger phpdoc

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,16 +10,6 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 /**
  * Class Controller
  * @package App\Http\Controllers
- *
- * @SWG\Info(title="Barq Backend", version="1")
- * @SWG\Swagger(
- *     schemes={"http", "https"},
- *     host="52.29.57.93",
- *     basePath="/api",
- *     consumes={"application/json"},
- *     produces={"application/json"},
- * )
- * @SWG\SecurityScheme(securityDefinition="api_key", type="apiKey", in="header", name="Authorization")
  */
 class Controller extends BaseController
 {
