7da435fb6c90e82c7b8593f42b73b98597aaec1c
Antony Repin <lehcode@gmail.com>
2017-08-14 19:13:36 +0300

    Updated navigation response structure

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -8,42 +8,46 @@
 namespace App\Http\Controllers;
 
 
+use App\Http\Responses\Admin\AdminResponse;
+use App\Repositories\User\UserRepository;
+use App\Services\Responder\ResponderService;
+use App\Services\UserService\UserService;
 use Barryvdh\Debugbar\Controllers\BaseController;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 
-class AdminController extends BaseController
+class AdminController
 {
 
 	use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
 	/**
-	 * @var UserRepositoryInterface
+	 * @var UserRepository
 	 */
 	protected $userRepository;
 
 	/**
-	 * @var ResponderServiceInterface
+	 * @var ResponderService
 	 */
 	protected $responderService;
 
 	/**
-	 * @var UserServiceInterface
+	 * @var UserService
 	 */
 	protected $userService;
 
 	/**
 	 * AdminController constructor.
 	 *
-	 * @param UserRepositoryInterface   $userRepository
-	 * @param ResponderServiceInterface $responderService
-	 * @param UserServiceInterface      $userService
+	 * @param UserRepository   $userRepository
+	 * @param ResponderService $responderService
+	 * @param UserService      $userService
 	 */
 	public function __construct(
-		UserRepositoryInterface $userRepository,
-		ResponderServiceInterface $responderService,
-		UserServiceInterface $userService
+		UserRepository $userRepository,
+		ResponderService $responderService,
+		UserService $userService
 	) {
 	
 		$this->userRepository = $userRepository;
@@ -59,74 +63,80 @@ class AdminController extends BaseController
 		return $this->responderService->response([
 			'status' => 'success',
 			'data' => [
-				'data' => [
-					[
-						'title' => 'Dashboard',
-						'icon' => 'action:ic_dashboard_24px',
-						'sref' => '/dashboard',
-					],
-					[
-						'title' => 'Carriers',
-						'icon' => '/img/material-design-icons/taxi.svg',
-						'sref' => '/carriers',
-					],
-					[
-						'title' => 'Customers',
-						'icon' => 'social:ic_group_24px',
-						'sref' => '/customers',
-					],
-					[
-						'title' => 'Orders',
-						'icon' => 'content:ic_next_week_24px',
-						'sref' => '/orders',
-					],
-					[
-						'title' => 'Trips',
-						'icon' => 'maps:ic_zoom_out_map_24px',
-						'sref' => '/trips',
-					],
-					[
-						'title' => 'Shipments',
-						'icon' => 'social:ic_party_mode_24px',
-						'sref' => '/shipments',
-					],
-					[
-						'title' => 'Cities',
-						'icon' => 'social:ic_domain_24px',
-						'sref' => '/cities',
-					],
-					[
-						'title' => 'Administrators',
-						'icon' => '/img/material-design-icons/account-settings-variant.svg',
-						'sref' => '/administrators',
-					],
-					[
-						'title' => 'Audit',
-						'icon' => '/img/material-design-icons/grease-pencil.svg',
-						'sref' => '/audit',
-					],
-					[
-						'title' => 'Payments',
-						'icon' => '/img/material-design-icons/cash-multiple.svg',
-						'sref' => '/payments',
-					],
-					[
-						'title' => 'Reports',
-						'icon' => 'action:ic_speaker_notes_24px',
-						'sref' => '/reports',
-					],
-					[
-						'title' => 'Notifications',
-						'icon' => 'communication:ic_chat_24px',
-						'sref' => '/notifications',
-					],
-					[
-						'title' => 'Settings',
-						'icon' => '/img/material-design-icons/toggle-switch-off.svg',
-						'sref' => '/settings',
-					],
-				]
+				[
+					'title' => 'Dashboard',
+					'icon' => 'action:ic_dashboard_24px',
+					'sref' => 'dashboard',
+				],
+				[
+					'title' => 'Carriers',
+					'icon' => '/img/material-design-icons/taxi.svg',
+					'sref' => 'carriers',
+				],
+				[
+					'title' => 'Customers',
+					'icon' => 'social:ic_group_24px',
+					'sref' => 'customers',
+				],
+				[
+					'title' => 'Orders',
+					'icon' => 'content:ic_next_week_24px',
+					'sref' => 'orders',
+				],
+				[
+					'title' => 'Trips',
+					'icon' => 'maps:ic_zoom_out_map_24px',
+					'sref' => 'trips',
+				],
+				[
+					'title' => 'Shipments',
+					'icon' => 'social:ic_party_mode_24px',
+					'sref' => 'shipments',
+				],
+				[
+					'title' => 'Cities',
+					'icon' => 'social:ic_domain_24px',
+					'sref' => 'cities',
+				],
+				[
+					'title' => 'Administrators',
+					'icon' => '/img/material-design-icons/account-settings-variant.svg',
+					'sref' => 'administrators',
+				],
+				[
+					'title' => 'Audit',
+					'icon' => '/img/material-design-icons/grease-pencil.svg',
+					'sref' => 'audit',
+				],
+				[
+					'title' => 'Payments',
+					'icon' => '/img/material-design-icons/cash-multiple.svg',
+					'sref' => 'payments',
+				],
+				[
+					'title' => 'Reports',
+					'icon' => 'action:ic_speaker_notes_24px',
+					'sref' => 'reports',
+				],
+				[
+					'title' => 'Notifications',
+					'icon' => 'communication:ic_chat_24px',
+					'sref' => 'notifications',
+				],
+				[
+					'title' => 'Settings',
+					'icon' => '/img/material-design-icons/toggle-switch-off.svg',
+					'sref' => 'settings',
+				],
 			]
 		]);
+	}
+
+	/**
+	 * @return \Illuminate\Http\JsonResponse
+	 * @throws \Exception
+	 */
+	public function all(){
+		return $this->responderService->fractal($this->userService->getAdmins(), AdminResponse::class);
 	}
 }
